90a133381abbbfcc27e2e9ac56813a2f8bc31202
youssef <youssefsayed22443@gmail.com>
2025-02-06 19:13:56 +0200

    initial files

---
+++ main.js
@@ -0,0 +1,23 @@
+
+const checkBtn = document.getElementById("check-btn");
+const result = document.getElementById("result");
+const checkPalindorm = () => {
+
+  const textInputValue = document.getElementById("text-input").value;
+  const word = textInputValue.replace(/[!@#$%^&*()_+={}\[\]:;"'<>,.?\/\\|-\s]/g,"");
+  const reversedWord = word.split('').reverse().join('');
+
+  let resultText;
+    if(word.length === 0){
+    alert("Please input a value");
+  }else{
+     if(word.toLowerCase() === reversedWord.toLowerCase()){
+    resultText = `${textInputValue} is a palindrome`
+    }else{
+    resultText = `${textInputValue} is not a palindrome`
+    }
+    result.innerText = resultText;
+  }
+}
+
+checkBtn.addEventListener("click",checkPalindorm);
