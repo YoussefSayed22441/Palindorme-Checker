f5e4363474903ef0e373dca82d7d592269f62958
youssef <youssefsayed22443@gmail.com>
2025-02-06 19:59:19 +0200

    added more interactivity to input field => value is checked when press enter

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 const checkBtn = document.getElementById("check-btn");
 const result = document.getElementById("result");
+const inputEl = document.getElementById("text-input");
 const checkPalindorm = () => {
 
   const textInputValue = document.getElementById("text-input").value;
@@ -12,12 +13,18 @@ const checkPalindorm = () => {
     alert("Please input a value");
   }else{
      if(word.toLowerCase() === reversedWord.toLowerCase()){
-    resultText = `${textInputValue} is a palindrome`
+    resultText = `${textInputValue} is a palindrome`;
     }else{
-    resultText = `${textInputValue} is not a palindrome`
+    resultText = `${textInputValue} is not a palindrome`;
     }
     result.innerText = resultText;
   }
 }
 
 checkBtn.addEventListener("click",checkPalindorm);
+inputEl.addEventListener("keydown",(e)=>{
+  if(e.key === "Enter"){
+    checkPalindorm();
+    console.log(e)
+  }
+});
